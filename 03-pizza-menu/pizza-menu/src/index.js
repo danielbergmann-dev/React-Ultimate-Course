610789a54ec1423994df6396ea5bd36452c20941
Daniel Bergmann <dhetzer3@gmail.com>
2024-11-11 21:21:09 +0100

    35. Components as Building Blocks

---
+++ 03-pizza-menu/pizza-menu/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+function App() {
+
+    
+    return (
+        <div>
+        <h1>Pizza Menu</h1>
+        </div>
+    );
+    }
+
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+    root.render(<App />);
