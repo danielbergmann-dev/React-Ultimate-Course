416f29b506b54a6e49a92eb80926bee5b03afa65
Daniel Bergmann <dhetzer3@gmail.com>
2024-11-22 11:04:52 +0100

    climate school

---
+++ Kompi/kompetenzraster-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import DragDropForm from './DragDropForm';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Kompetenzraster Bewertungsbogen</h1>
+      <DragDropForm />
+    </div>
+  );
+}
+
+export default App;
