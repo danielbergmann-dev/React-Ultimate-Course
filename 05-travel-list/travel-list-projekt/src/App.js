7ae1888f400c6552cc6f9039d04fd11dc010070b
Daniel Bergmann <dhetzer3@gmail.com>
2024-12-02 09:51:48 +0100

    73

--- 05-travel-list/travel-list-projekt/src/App.js
+++ 05-travel-list/travel-list-projekt/src/App.js
@@ -20,8 +20,14 @@ function Logo() {
 }
 
 function Form() {
+
+function handleSubmit(e) {
+  e.preventDefault();
+  console.log(e);
+}
+
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}> {/* hier damit auch die return taste gedrückt werden kann */ }
       <h3>What do you need for your 😍 trip?</h3>
       <select>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
