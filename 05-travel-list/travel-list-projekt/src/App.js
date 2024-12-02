cc754be0fe82b1878d2e800d1596ca85b6016710
Daniel Bergmann <dhetzer3@gmail.com>
2024-12-02 13:34:57 +0100

    73.Controlled Elements

--- 05-travel-list/travel-list-projekt/src/App.js
+++ 05-travel-list/travel-list-projekt/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
@@ -21,6 +23,8 @@ function Logo() {
 
 function Form() {
 
+  const [description, setDescription] = useState("");
+
 function handleSubmit(e) {
   e.preventDefault();
   console.log(e);
@@ -36,7 +40,7 @@ function handleSubmit(e) {
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input type="text" placeholder="Item..." value={description} onChange={(e) => setDescription(e.target.value)}/>
       <button>Add</button>
     </form>
   );
